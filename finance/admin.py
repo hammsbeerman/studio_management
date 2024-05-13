162a707c3618c9ef0358be88b3fe2ae7ff030233
Adam Kaney <adam.kaney@gmail.com>
2024-05-13 20:04:19 +0000

    update invoice number

--- finance/admin.py
+++ finance/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
-from .models import Payments, Araging
+from .models import Payments, Araging, AccountsPayable
 
 admin.site.register(Payments)
 
 admin.site.register(Araging)
+
+admin.site.register(AccountsPayable)
